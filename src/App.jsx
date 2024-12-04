f969aef3251105a8503b2fa5f5430098bb0f2afc
Riajul Islam <riajulpislam@gmail.com>
2024-12-04 20:04:19 +0600

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./Navigation/Nav"
+import Products from "./Products/Products"
+import Recommended from "./Recommended/Recommended"
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Products/>
+      <Recommended/>
+    </div>
+  )
+}
+
+export default App
