8bf835b824fd92cb466fd65bef8490e0ff584b55
Riajul Islam <riajulpislam@gmail.com>
2024-12-04 21:28:14 +0600

    navigation-section-complete

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,8 +1,21 @@
 
+import { FiHeart } from "react-icons/fi"
 import "./Nav.css"
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai"
+
 function Nav() {
   return (
-    <div>Nav</div>
+   <nav>
+     <div className="nav_container">
+      <input className="search-input" type="text" placeholder="Enter your search shoes" />
+     </div>
+     <div className="profile-container">
+      <a href="#"><FiHeart className="nav-icons"/></a>
+      <a href="#"><AiOutlineShoppingCart className="nav-icons"/></a>
+      <a href="#"><AiOutlineUserAdd className="nav-icons"/></a>
+      
+     </div>
+   </nav>
   )
 }
 
